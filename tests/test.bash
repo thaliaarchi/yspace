3b69aa44e5b87a504f2e7f9e5307d04f6c254d85
Andrew Archibald <andrew@aarchibald.com>
2021-06-26 00:17:27 -0700

    Test laziness and arguments for slide and copy

--- tests/test.bash
+++ tests/test.bash
@@ -4,12 +4,19 @@ wspace bad_filename.ws
 wspace empty_file.ws
 wspace implicit_end.ws
 
+wspace encoding_utf8_bom.ws && echo
+wspace encoding_invalid_utf8.ws && echo
+wspace encoding_invalid_utf8_lazy.ws && echo
+wspace unknown_instruction.ws
+
 wspace underflow_dup.ws
 wspace underflow_copy.ws
 wspace underflow_copy_unused.ws
 wspace underflow_swap.ws
 wspace underflow_drop.ws
 wspace underflow_slide.ws
+wspace underflow_slide_zero.ws
+wspace underflow_slide_negative.ws
 wspace underflow_add.ws
 wspace underflow_sub.ws
 wspace underflow_mul.ws
@@ -25,13 +32,15 @@ wspace underflow_printi.ws
 wspace underflow_readc.ws
 wspace underflow_readi.ws
 
-wspace encoding_utf8_bom.ws && echo
-wspace encoding_invalid_utf8.ws && echo
-wspace encoding_invalid_utf8_lazy.ws && echo
-wspace unknown_instruction.ws
-
 wspace push_zero_empty.ws
 wspace push_zero_empty_unused.ws
+
+wspace copy_negative.ws
+wspace copy_negative_unused.ws
+
+wspace slide_zero.ws && echo
+wspace slide_negative.ws && echo
+wspace slide_less.ws && echo
 
 wspace div_zero.ws
 wspace mod_zero.ws
